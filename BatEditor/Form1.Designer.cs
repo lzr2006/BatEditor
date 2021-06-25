8835bc5c7a8f25785fe27db66589724eb381d2ce
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2021-06-25 11:47:53 +0800

    Add files via upload

---
+++ BatEditor/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace BatEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.染色速度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.减5秒ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.调试ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.背景风格ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.夜空ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.白日ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.调试ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(752, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.关闭ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(164, 30);
+            this.打开ToolStripMenuItem.Text = "打开文件";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(164, 30);
+            this.保存ToolStripMenuItem.Text = "保存文件";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(164, 30);
+            this.关闭ToolStripMenuItem.Text = "关闭文件";
+            this.关闭ToolStripMenuItem.Click += new System.EventHandler(this.关闭ToolStripMenuItem_Click);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.染色速度ToolStripMenuItem,
+            this.背景风格ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.设置ToolStripMenuItem.Text = "设置";
+            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.设置ToolStripMenuItem_Click);
+            // 
+            // 染色速度ToolStripMenuItem
+            // 
+            this.染色速度ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.减5秒ToolStripMenuItem});
+            this.染色速度ToolStripMenuItem.Name = "染色速度ToolStripMenuItem";
+            this.染色速度ToolStripMenuItem.Size = new System.Drawing.Size(164, 30);
+            this.染色速度ToolStripMenuItem.Text = "染色速度";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(139, 30);
+            this.toolStripMenuItem2.Text = "加5秒";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // 减5秒ToolStripMenuItem
+            // 
+            this.减5秒ToolStripMenuItem.Name = "减5秒ToolStripMenuItem";
+            this.减5秒ToolStripMenuItem.Size = new System.Drawing.Size(139, 30);
+            this.减5秒ToolStripMenuItem.Text = "减5秒";
+            this.减5秒ToolStripMenuItem.Click += new System.EventHandler(this.减5秒ToolStripMenuItem_Click);
+            // 
+            // 调试ToolStripMenuItem
+            // 
+            this.调试ToolStripMenuItem.Name = "调试ToolStripMenuItem";
+            this.调试ToolStripMenuItem.Size = new System.Drawing.Size(58, 28);
+            this.调试ToolStripMenuItem.Text = "调试";
+            this.调试ToolStripMenuItem.Click += new System.EventHandler(this.调试ToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(728, 451);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "打开文件";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(16, 501);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "关键字染色：";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(124, 495);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(616, 29);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 100;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // 背景风格ToolStripMenuItem
+            // 
+            this.背景风格ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.夜空ToolStripMenuItem,
+            this.白日ToolStripMenuItem});
+            this.背景风格ToolStripMenuItem.Name = "背景风格ToolStripMenuItem";
+            this.背景风格ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.背景风格ToolStripMenuItem.Text = "背景风格";
+            // 
+            // 夜空ToolStripMenuItem
+            // 
+            this.夜空ToolStripMenuItem.Name = "夜空ToolStripMenuItem";
+            this.夜空ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.夜空ToolStripMenuItem.Text = "夜空";
+            this.夜空ToolStripMenuItem.Click += new System.EventHandler(this.夜空ToolStripMenuItem_Click);
+            // 
+            // 白日ToolStripMenuItem
+            // 
+            this.白日ToolStripMenuItem.Name = "白日ToolStripMenuItem";
+            this.白日ToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.白日ToolStripMenuItem.Text = "白日";
+            this.白日ToolStripMenuItem.Click += new System.EventHandler(this.白日ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(752, 528);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MethodBox BatEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 染色速度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 减5秒ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem 调试ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 背景风格ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 夜空ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 白日ToolStripMenuItem;
+    }
+}
+
---
+++ BatEditor/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace BatEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int rs = 10000;
+        public string pt;
+        public int fg = 1;
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+
+            pt = openFileDialog1.FileName;
+            Text = "MethodBox BatEditor  *" + pt;
+
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void 关闭ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Text = "MethodBox BatEditor";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string key = "net";
+            Regex reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            MatchCollection mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "start";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "goto";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "shutdown";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "regsvr32";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "sfc";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "netstat";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            key = "ping";
+            reg = new Regex(@"(?i)\b" + Regex.Escape(key) + @"\b");
+            mc = reg.Matches(richTextBox1.Text);
+            foreach (Match m in mc)
+            {
+                richTextBox1.SelectionStart = m.Index;
+                richTextBox1.SelectionLength = m.Length;
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+
+            progressBar1.Value = 100;
+
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 设置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            染色速度ToolStripMenuItem.Text = "染色速度" + (rs / 1000).ToString() + "秒每次";
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            rs = rs + 5000;
+            染色速度ToolStripMenuItem.Text = "染色速度" + (rs / 1000).ToString() + "秒每次";
+            timer1.Interval = rs;
+        }
+
+        private void 减5秒ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rs > 5000)
+            {
+                rs = rs - 5000;
+                染色速度ToolStripMenuItem.Text = "染色速度" + (rs / 1000).ToString() + "秒每次";
+                timer1.Interval = rs;
+            }
+            else
+            {
+                MessageBox.Show("不能再减小了！","达到最大速度",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //进度条
+            try
+            {
+                progressBar1.Value = progressBar1.Value - rs / 1000;
+            }
+            catch
+            {
+                progressBar1.Value = 12;
+            }
+        }
+
+        private void 调试ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            test(pt);
+        }
+
+        public string test(string str)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+            p.StandardInput.WriteLine(str + "&exit");
+            p.StandardInput.AutoFlush = true;
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+            richTextBox1.Text = richTextBox1.Text + "\n\n\n\n调试结果:\n\n" + output;
+            return output;
+        }
+
+
+        private void 夜空ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.BackColor = BackColor = Color.Black;
+            menuStrip1.BackColor = Color.Gray;
+            label1.ForeColor = Color.White;
+            richTextBox1.ForeColor = Color.White;
+        }
+
+        private void 白日ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.BackColor = BackColor = Color.White;
+            menuStrip1.BackColor = Color.Gray;
+            label1.ForeColor = Color.Black;
+            richTextBox1.ForeColor = Color.Black;
+        }
+    }
+}
